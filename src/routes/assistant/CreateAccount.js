a9bf6c1e1bff692f01329294392f269c824665b4
febinurfajar19 <febinurfajar13@gmail.com>
2022-06-11 18:14:27 +0700

    feat: Create Sign Up for Assistant Page

--- src/routes/assistant/CreateAccount.js
+++ src/routes/assistant/CreateAccount.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const LogInOption = () => {
   return (
     <div>
-        <h1>Sing Up</h1>
+      <h1>
+        <center>VLab</center>
+      </h1>
+      <h3>Lengkapi Profil Anda</h3>
+      <form>
+        <label>Username</label>
+        <input type="text" name="username" placeholder="Username" />
+        <label>Email</label>
+        <input type="text" name="email" placeholder="Email" />
+        <label>Sandi</label>
+        <input type="password" name="sandi" placeholder="Sandi" />
+        <label>Konfirmasi Sandi</label>
+        <input type="password" name="sandi" placeholder="Konfirmasi Sandi" />
+        <button type="submit">
+          <Link to="/login">Daftar</Link>
+        </button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default LogInOption
+export default LogInOption;
