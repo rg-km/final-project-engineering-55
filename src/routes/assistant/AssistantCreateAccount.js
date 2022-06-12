2e21a9b72364a618e95e310e0f8e94db1aa59059
febinurfajar19 <febinurfajar13@gmail.com>
2022-06-12 10:21:58 +0700

    style: add style to the sign-up-for-assistant page

--- src/routes/assistant/AssistantCreateAccount.js
+++ src/routes/assistant/AssistantCreateAccount.js
@@ -1,28 +1,84 @@
 import React from "react";
+import {
+  Card,
+  Col,
+  Row,
+  Form,
+  Button,
+  FloatingLabel,
+  Container,
+} from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 const AssistantCreateAccount = () => {
   return (
-    <div>
-      <h1>
+    <Container>
+      <h1 className="mt-3">
         <center>VLab</center>
       </h1>
-      <h3>Lengkapi Profil Anda</h3>
-      <form>
-        <label>Username</label>
-        <input type="text" name="username" placeholder="Username" />
-        <label>Email</label>
-        <input type="text" name="email" placeholder="Email" />
-        <label>Sandi</label>
-        <input type="password" name="sandi" placeholder="Sandi" />
-        <label>Konfirmasi Sandi</label>
-        <input type="password" name="sandi" placeholder="Konfirmasi Sandi" />
-        <Link to="/login">
-          <button type="submit">Daftar
-          </button>
-        </Link>
-      </form>
-    </div>
+      <Row className="justify-content-center">
+        <Col xs={10} md={4}>
+          <Card className="my-5 px-5 mx-3 py-3">
+            <h4 className="mb-4 text-center">Lengkapi Profil Anda</h4>
+            <Form.Group>
+              <FloatingLabel
+                controlId="floatingInput"
+                label="Username"
+                className="mb-3"
+              >
+                <Form.Control
+                  type="text"
+                  placeholder="Username"
+                  name="username"
+                />
+              </FloatingLabel>
+            </Form.Group>
+            <Form.Group>
+              <FloatingLabel
+                controlId="floatingEmail"
+                label="Email"
+                className="mb-3"
+              >
+                <Form.Control type="email" placeholder="Email" name="email" />
+              </FloatingLabel>
+            </Form.Group>
+            <Form.Group>
+              <FloatingLabel
+                controlId="floatingPassword"
+                label="Password"
+                className="mb-3"
+              >
+                <Form.Control
+                  type="password"
+                  placeholder="Sandi"
+                  name="password"
+                />
+              </FloatingLabel>
+            </Form.Group>
+            <Form.Group>
+              <FloatingLabel
+                controlId="floatingPassword"
+                label="Konfirmasi Password"
+                className="mb-3"
+              >
+                <Form.Control
+                  type="password"
+                  placeholder="Konfirmasi Sandi"
+                  name="confirmPassword"
+                />
+              </FloatingLabel>
+            </Form.Group>
+            <div class="col-md-12 text-center">
+              <Link to="/login">
+                <Button variant="primary" type="submit">
+                  Daftar
+                </Button>
+              </Link>
+            </div>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
