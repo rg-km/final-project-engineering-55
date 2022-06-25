eaf081dc0a23dbaaa347c7f16bd8983b13498a1a
Amin Abdillah <andiabdillah255@gmail.com>
2022-06-25 21:49:52 +0700

    style: fix navbar styling

--- frontend/src/routes/assistant/components/AssistantNavbar.js
+++ frontend/src/routes/assistant/components/AssistantNavbar.js
@@ -1,28 +1,52 @@
 import React from "react";
 import { Container, Dropdown, DropdownButton, Form, Nav, Navbar } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import AssistantNavIcon from "./AssistantNavIcon";
+
+const style = (isActive) => ({
+  fontWeight: isActive ? 600 : 400,
+  color: isActive ? "gold" : "white",
+  paddingBlock: 8,
+  paddingInline: 16,
+  borderRadius: 8,
+  textDecoration: isActive ? "underline" : "none",
+});
+
+const PATHS = [
+  { to: "/assistant/main/home", label: "Home" },
+  { to: "/assistant/main/classes", label: "Kelas" },
+];
 
 const AssistantNavbar = () => {
     return (
         <Navbar bg="dark" variant="dark" expand="lg">
           <Container xs={4} md={4}>
-            <Navbar.Brand>VLab</Navbar.Brand>
+            <Navbar.Brand
+              style={{fontSize: "35px", fontWeight: 600}}
+            >VLab</Navbar.Brand>
             <Navbar.Toggle aria-controls="navbarScroll" />
             <Navbar.Collapse id="navbarScroll">
               <Nav
-                className="me-auto my-2 my-lg-0"
-                style={{ maxHeight: '100px' }}
+                className="me-auto my-lg-0"
+                style={{ maxHeight: '100px', marginLeft:"100px", fontSize:"28px" }}
                 navbarScroll
               >
-                <Nav.Link as={Link} to={"/assistant/main/home"} >Home</Nav.Link>
-                <Nav.Link as={Link} to={"/assistant/main/classes"} >Kelas</Nav.Link>
+                {PATHS.map((path) => (
+                  <NavLink
+                    key={path.to}
+                    to={path.to}
+                    style={({ isActive }) => style(isActive)}
+                  >
+                    {path.label}
+                  </NavLink>
+                ))}
               </Nav>
               <Form className="d-flex">
                 <DropdownButton
                   id="dropdown-basic-button"
                   title="Logout"
                   variant="primary"
+                  style={{ marginRight: "10px"}}
                   >
                   <Dropdown.Item as={Link} to={"/"} >Logout?</Dropdown.Item>
                 </DropdownButton>
--- frontend/src/routes/student/components/StudentClassNavbar.js
+++ frontend/src/routes/student/components/StudentClassNavbar.js
@@ -1,32 +1,57 @@
 import React from 'react'
 import { Container, Dropdown, DropdownButton, Form, Nav, Navbar } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import StudentNavIcon from './StudentNavIcon';
+
+const style = (isActive) => ({
+  fontWeight: isActive ? 600 : 400,
+  color: isActive ? "gold" : "white",
+  paddingBlock: 8,
+  paddingInline: 16,
+  borderRadius: 8,
+  textDecoration: isActive ? "underline" : "none",
+});
+
+const PATHS = [
+  { to: "/student/main/classes/posts", label: "Forum" },
+  { to: "/student/main/classes/assignments", label: "Tugas Kelas" },
+  { to: "/student/main/classes/quiz", label: "Kuis" },
+  { to: "/student/main/classes/members", label: "Anggota" },
+];
 
 const StudentClassNavbar = () => {
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
       <Container xs={4} md={4}>
-        <Link to="/student/main/classes" style={{textDecoration: 'none'}}>
-          <Navbar.Brand>Kembali</Navbar.Brand>
+        <Link to="/student/main/classes"
+            style={{textDecoration: 'none'}}>
+          <Navbar.Brand
+            style={{fontSize: "35px", fontWeight: 600}}>
+          Kembali</Navbar.Brand>
         </Link>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
           <Nav
             className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
+            style={{ maxHeight: '100px', marginLeft:"100px", fontSize:"28px" }}
             navbarScroll
           >
-            <Nav.Link as={Link} to={"/student/main/classes/posts"} >Forum</Nav.Link>
-            <Nav.Link as={Link} to={"/student/main/classes/assignments"} >Tugas Kelas</Nav.Link>
-            <Nav.Link as={Link} to={"/student/main/classes/quiz"} >Kuis</Nav.Link>
-            <Nav.Link as={Link} to={"/student/main/classes/members"} >Anggota</Nav.Link>
+            {PATHS.map((path) => (
+              <NavLink
+                key={path.to}
+                to={path.to}
+                style={({ isActive }) => style(isActive)}
+              >
+                {path.label}
+              </NavLink>
+            ))}
           </Nav>
           <Form className="d-flex">
             <DropdownButton
               id="dropdown-basic-button"
               title="Logout"
               variant="primary"
+              style={{ marginRight: "10px"}}
               >
               <Dropdown.Item as={Link} to={"/"} >Logout?</Dropdown.Item>
             </DropdownButton>
