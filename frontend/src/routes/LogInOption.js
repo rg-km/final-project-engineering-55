65d630b53520ec130b11b4683aead3f78ec604b3
febinurfajar19 <febinurfajar13@gmail.com>
2022-06-26 17:59:14 +0700

    fix: fix login page style

--- frontend/src/routes/LogInOption.js
+++ frontend/src/routes/LogInOption.js
@@ -1,40 +1,50 @@
 import React from "react";
 import { Container, Card, Row, Col, Nav } from "react-bootstrap";
+import Login from "../assets/Login.jpg";
 import { Link, Outlet } from "react-router-dom";
 import NavLogo from "../components/NavLogo";
 
 const LogInOption = () => {
   return (
     <>
-    <NavLogo />
-    <Container>
-      <h3 className="mt-5 mb-3">
-        <center>Masuk Sebagai</center>
-      </h3>
-      <Row className="justify-content-center">
-        <Col xs={10} md={4}>
-          <Card className="mx-2 shadow">
-            <Card.Header>
-              <Nav fill variant="pills">
-                <Nav.Item>
-                  <Nav.Link href="#" as={Link} to="/login/assistant">
-                    Asisten
-                  </Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link href="##" as={Link} to="/login/student">
-                    Murid
-                  </Nav.Link>
-                </Nav.Item>
-              </Nav>
-            </Card.Header>
-            <Card.Body className="px-1">
-              <Outlet />
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-    </Container>
+      <NavLogo />
+      <Container>
+        <Row className="justify-content-center">
+          <Col>
+            <img
+              src={Login}
+              alt="login-page"
+              width={500}
+              height={500}
+              style={{ marginLeft: "3%" }}
+            />
+          </Col>
+          <Col xs={10} md={4} style={{ marginRight: "7%" }}>
+            <h4 className="mt-5 mb-3">
+              <center>Masuk Sebagai</center>
+            </h4>
+            <Card className="mx-3">
+              <Card.Header>
+                <Nav fill variant="pills">
+                  <Nav.Item>
+                    <Nav.Link href="#" as={Link} to="/login/assistant">
+                      Asisten
+                    </Nav.Link>
+                  </Nav.Item>
+                  <Nav.Item>
+                    <Nav.Link href="##" as={Link} to="/login/student">
+                      Murid
+                    </Nav.Link>
+                  </Nav.Item>
+                </Nav>
+              </Card.Header>
+              <Card.Body className="px-1">
+                <Outlet />
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 };
